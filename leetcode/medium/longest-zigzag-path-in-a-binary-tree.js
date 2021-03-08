cdb3aa83f26e87b3d555efc20edcdf64054bc189
Simone <s.caserini@reply.it>
2021-03-08 10:43:26 +0100

    :tada: add solutions

--- leetcode/medium/longest-zigzag-path-in-a-binary-tree.js
+++ leetcode/medium/longest-zigzag-path-in-a-binary-tree.js
@@ -7,6 +7,7 @@
  * }
  */
 /**
+ * BFS solution
  * @param {TreeNode} root
  * @return {number}
  */
@@ -16,7 +17,6 @@ var longestZigZag = function (root) {
   root.left && queue.push([root.left, 1, 'left']);
   root.right && queue.push([root.right, 1, 'right']);
 
-  // BFS
   while (queue.length > 0) {
     let [node, curr, direction] = queue.shift();
     if (curr > max) {
@@ -32,4 +32,40 @@ var longestZigZag = function (root) {
   }
 
   return max;
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * DFS solution
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var longestZigZag = function (root) {
+  let res = { max: 0 };
+  longestZigZagRecursive(root, 0, '', res);
+  return res.max;
+};
+
+const longestZigZagRecursive = (root, cur = 0, direction = '', res) => {
+  if (cur > res.max) {
+    res.max = cur;
+  }
+
+  for (let dir of ['left', 'right']) {
+    if (direction == '') {
+      root.left && longestZigZagRecursive(root.left, 1, 'left', res);
+      root.right && longestZigZagRecursive(root.right, 1, 'right', res);
+    } else if ((direction == 'left' && dir == 'left') || (direction == 'right' && dir == 'right')) {
+      root[dir] && longestZigZagRecursive(root[dir], 1, dir, res);
+    } else {
+      root[dir] && longestZigZagRecursive(root[dir], 1 + cur, dir, res);
+    }
+  }
 };
