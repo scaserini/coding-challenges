a18b0472e404a7f822d1614c23bbc0728ef60f96
Simone <s.caserini@reply.it>
2021-03-24 17:03:25 +0100

    :tada: add solutions

---
+++ leetcode/medium/course-schedule.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function (numCourses, prerequisites) {
+  /* Basically, find if a cycle exists in the graph */
+
+  // visited nodes array
+  let visited = Array(numCourses).fill(0);
+  // build adjacency list
+  let adjList = {};
+  for (let [course, prerequisite] of prerequisites) {
+    if (!adjList[course]) {
+      adjList[course] = [];
+    }
+    adjList[course].push(prerequisite);
+  }
+
+  for (let course in adjList) {
+    if (visited[course] == 0 && !dfs(adjList, course, visited)) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+function dfs(adjList, course, visited) {
+  if (visited[course] == -1) {
+    return false;
+  }
+
+  // mark current visiting nodes with -1
+  visited[course] = -1;
+
+  if (adjList[course]) {
+    for (let c of adjList[course]) {
+      if (visited[c] != 1 && !dfs(adjList, c, visited)) {
+        return false;
+      }
+    }
+  }
+
+  visited[course] = 1;
+  return true;
+}
