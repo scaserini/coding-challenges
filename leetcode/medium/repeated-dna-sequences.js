e0753851eb058617431c49eec09af90db4545248
Simone <s.caserini@reply.it>
2021-03-25 11:13:06 +0100

    :sparkles: add solutions

---
+++ leetcode/medium/repeated-dna-sequences.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var findRepeatedDnaSequences = function (s) {
+  // In terms of performance and memory usage, Set() is better than a plain Object
+  // In this case - Runtime: 30% less, Memory: 25% less
+  let hashTable = new Set();
+  let result = new Set();
+  let left = 0;
+  let right = 10;
+
+  while (right <= s.length) {
+    let key = s.slice(left, right);
+    if (hashTable.has(key)) result.add(key);
+    hashTable.add(key);
+    right++;
+    left++;
+  }
+
+  return [...result];
+};
