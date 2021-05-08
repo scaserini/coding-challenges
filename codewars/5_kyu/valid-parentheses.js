80170a875b79ee8c6dbf3598d7f48a1d290478d7
Simone <s.caserini@reply.it>
2021-05-08 22:54:03 +0200

    :tada: update solution

--- codewars/5_kyu/valid-parentheses.js
+++ codewars/5_kyu/valid-parentheses.js
@@ -1,13 +1,10 @@
 function validParentheses(parens) {
   let stack = [];
-  for (paren of parens) {
-    if (paren == '(') {
-      stack.push('(');
-    } else if (paren == ')') {
-      if (stack.pop() != '(') {
-        return false;
-      }
-    }
+
+  for (let paren of parens) {
+    if (paren === '(') stack.push('(');
+    if (paren === ')' && stack.pop() !== '(') return false;
   }
-  return stack.length == 0;
+
+  return stack.length === 0;
 }
